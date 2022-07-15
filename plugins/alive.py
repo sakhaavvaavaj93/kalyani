a46f2cbe340e36534042b6f2f88d7a0607d2d142
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-07-15 16:48:39 +0530

    Add files via upload

---
+++ plugins/alive.py
@@ -0,0 +1,55 @@
+import asyncio
+from time import time
+from datetime import datetime
+from modules.helpers.filters import command
+from modules.helpers.command import commandpro
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+    
+   
+
+@Client.on_message(command("start") & filters.private & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/d0b2899c25498fb52d0c8.jpg",
+        caption=f"""**━━━━━━━━━━━━━━━━━━━━━━━━
+**"""
+    )
+    
+    
+@Client.on_message(commandpro(["/start", "/alive", "blackcat"]) & filters.group & ~filters.edited)
+async def start(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/d0b2899c25498fb52d0c8.jpg",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "ɴᴇᴛᴡᴏʀᴋ", url=f"https://t.me/Kerala_Cousinsofficial")
+                ]
+            ]
+        ),
+    )
+
