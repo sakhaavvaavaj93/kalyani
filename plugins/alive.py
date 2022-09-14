6449d49d46a6af172edb7b819911e867ef0a0887
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-09-14 12:53:44 +0530

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -33,19 +33,23 @@ async def _human_time_duration(seconds):
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/82cea8cd9e2cfa08b8f3e.jpg",
+        photo=f"http://telegra.ph/file/de61130f75f47984f0530.jpg",
         caption=f"""**
 『It's a Music bot without lag and struck .
-  It's a official Music bot of @kk_kovilakam 
+  It's a official Music bot of @KanimangalamKovilakam 
 Nb : Bot and Userbot are locked by owner ,
      who wish to add this bot to your group,
-     then , contact @CRUAL_MIND』
+     then , contact @kk_heaven_hater』
 **""",
 reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "『🅖🅡🅞🅤🅟』", url=f"https://t.me/Kerala_Cousinsofficial")
+                        "『𝗖𝗛𝗔𝗡𝗡𝗘𝗟』", url=f"https://t.me/KK_LINKS"),
+                    InlineKeyboardButton(
+                        "『𝗜𝗡𝗦𝗧𝗔🎭𝗚𝗥𝗔𝗠』", url=f"https://www.instagram.com/kani_mangalamkovilakam/"),
+                    InlineKeyboardButton(
+                        "『𝗙𝗔𝗖𝗘🌎𝗕𝗢𝗢𝗞』", url=f"https://www.facebook.com/profile.php?id=100085628503716") 
                 ]
             ]
         ),
@@ -55,13 +59,20 @@ reply_markup=InlineKeyboardMarkup(
 @Client.on_message(commandpro(["/start", "/alive", "blackcat"]) & filters.group & ~filters.edited)
 async def start(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/82cea8cd9e2cfa08b8f3e.jpg",
-        caption=f"""**ഇപ്പോഴും ജീവനോടെ ഉണ്ട് . ചത്തിട്ടില്ല**""",
+        photo=f"http://telegra.ph/file/de61130f75f47984f0530.jpg",
+        caption=f"""**𝙾𝙵𝙵𝙸𝙲𝙸𝙰𝙻 𝐌𝐔𝐒𝐈𝐂 Bot of 𓄂𝐊𝐊࿐ཽ༵✰**,
+        it's A 💯% LAG AND STRUCK FREE MUSIC BOT , IT KEEP CLEAN YOUR CHAT WHEN IT WORK ON YOUR CHAT,3hrs Unlimited Playing without Lag,
+        NB:- 24HRS ASSISTANCE OF bot
+             (for anytime help:- [ANIYATHIKUTTIEEDE_CHECHIKUTTI](https://t.me/ANIYATHIKUTTIEEDE_CHECHIKUTTI)""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "GrouP", url=f"https://t.me/Kerala_Cousinsofficial")
+                        "▲𝐅𝐀𝐌𝐈𝐋𝐘▼", url=f"https://t.me/KK_LINKS"),
+                    InlineKeyboardButton(
+                        "『𝐈𝐧𝐬𝐭𝐚🎭𝐆𝐫𝐚𝐦』", url=f"https://www.instagram.com/kani_mangalamkovilakam/"),
+                    InlineKeyboardButton(
+                        "『𝐅𝐀𝐂𝐄🌎𝐁𝐎𝐎𝐊』", url=f"https://www.facebook.com/profile.php?id=100085628503716")
                 ]
             ]
         ),
